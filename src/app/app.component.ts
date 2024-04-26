08f3a6dfe153b6b920f9a8833643d9bb3516d435
jkolowca <joanna.kolowca.mf13@gmail.com>
2024-04-26 09:29:57 +0200

    navigation fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './core/components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent],
   template: `
     <app-navigation />
     <router-outlet />
